86b3cfefa7daa9c449a5dfdaa35254d7a751aa4e
Amal Agrawal <amalagrawal@amals-mbp-2.bc.edu>
2019-09-16 17:33:37 -0400

    done

--- Dungeon Dice/ViewController.swift
+++ Dungeon Dice/ViewController.swift
@@ -10,13 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //make the dice
+    @IBOutlet weak var randomNumberLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func dice4(_ sender: UIButton) {
+        let side=4
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    
+    @IBAction func dice6(_ sender: Any) {
+        let side=6
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    @IBAction func dice8(_ sender: Any) {
+        let side=8
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    
+    @IBAction func dice10(_ sender: Any) {
+        let side=10
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    @IBAction func dice12(_ sender: Any) {
+        let side=12
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    @IBAction func dice20(_ sender: Any) {
+        let side=20
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    @IBAction func dice100(_ sender: Any) {
+        let side=100
+        let randomNumber = Int.random(in: 1...side)
+        randomNumberLabel.text = "You rolled a \(side) sided dice and got a \(randomNumber)"
+    }
+    
 }
 
